606665a3089bb112043ca5485b25c2e14bdfc170
chipmunk-dev <devquokkajeong@gmail.com>
2023-01-18 01:27:07 +0900

    feat: create post API that can create user

---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,39 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retrieveuser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
