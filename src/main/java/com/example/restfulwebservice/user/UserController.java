59e5d38072e8a9262992f5904204995a49f8c5ab
chipmunk-dev <devquokkajeong@gmail.com>
2023-01-19 01:24:44 +0900

    feat: create UserNotFoundException

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -22,7 +22,13 @@ public class UserController {
 
     @GetMapping(path = "/users/{id}")
     public User retrieveuser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
     }
 
     @PostMapping(path = "/users")
